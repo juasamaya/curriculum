349cc7691c6bd9ccd1300a4d505e7030c988b50a
juasamaya <juasamaya111@soy.sena.edu.co>
2025-08-21 15:44:20 -0500

    modifying vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/curriculum/' // 👈 necesario para GitHub Pages
+  plugins: [react()]👈 necesario para GitHub Pages
 })
