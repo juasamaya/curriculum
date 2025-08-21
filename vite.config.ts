f64e508cf8d52c078337d94aa4bb86c6e41b622d
juasamaya <juasamaya111@soy.sena.edu.co>
2025-08-21 15:44:39 -0500

    modifying vite.conmfig removing route

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()]👈 necesario para GitHub Pages
+  plugins: [react()]
 })
